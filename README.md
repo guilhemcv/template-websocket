1a8a0f394b12f2215a156acba1ca33f55242a017
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-06-20 11:37:16 +0200

    udpate README.md

--- README.md
+++ README.md
@@ -3,3 +3,35 @@
 Nous allons manipuler les websokets avec React & Node.js.
 
 Nous utilisons le paquage [socket.io](https://socket.io/) qui fait très bien le travail !
+
+
+## Backend
+
+Coté back nous allons faire un server `express` simple.
+
+Mais nous allons quand même faire quelques modification par rapport à d'habitude.
+
+nous allons utiliser un paquet de chez node `http` :
+
+```js
+const express = require("express");
+// pourquoi http ici ? car nous allons l'utiliser dans la création de 
+// notre server
+// qui va permettre de reutiliser le server avec socket.io 
+// avec la même instance de serveur HTTP !
+const http = require("http");
+
+const app = express();
+// ici
+const server = http.createServer(app);
+```
+
+## Mais comment ça marche ?
+
+![](./_doc/socketio.png)
+
+Avec l'image ci-dessus, j'espère que ça vous aide.
+
+C'est une communication bidirectionnelle entre le client et le serveur. On appel ça une communication full duplex.
+
+## Frontend
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
   const [messageList, setMessageList] = useState([]);
+  /**
+   * Structure d'un message
+   * {
+   *     author: "nickname",
+   *     text: "message"
+   *     id: currentUser
+   * }
+   */
   const [nickName, setNickName] = useState("");
   const [newMessageText, setNewMessageText] = useState("");
   const [socket, setSocket] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    //!TO DO: submit message to server
   };
+
+  // Au montage de la page
+  useEffect(() => {
+    //!TO DO création de la connexion avec le serveur
+  }, []);
+
+  // Mise à jour de la liste des messages
+  useEffect(() => {
+    //!TO DO: récupération des messages du serveur
+  }, [messageList]);
 
   return (
     <div className="App">
       <h2>Messages</h2>
-      {messageList.map((message) => {
+      {messageList.map((message, id) => {
         return (
-          <div key={message.id}>
-            {message.author} : {message.text}
-          </div>
+          <p
+            key={id}
+            className={message.id === currentUser ? "my-message" : "message"}
+          >
+            <strong>{message.author}</strong>: {message.text}
+          </p>
         );
       })}
-
       <form onSubmit={handleSubmit}>
         <h2>New Message</h2>
         <input
